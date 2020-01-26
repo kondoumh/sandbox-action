9e435867d998fb4db71dd86346d831569d504233
kondoumh <kondoumh@gmail.com>
2020-01-26 19:11:17 +0900

    Add process env

--- index.test.js
+++ index.test.js
@@ -1,7 +1,9 @@
+const process = require('process');
 const cp = require('child_process');
 const path = require('path');
 
 test('test runs', () => {
+  process.env['INPUT_WHO-TO-GREET'] = 'Hoge';
   const ip = path.join(__dirname, 'dist/index.js');
   console.log(cp.execSync(`node ${ip}`).toString());
 })
